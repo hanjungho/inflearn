e57d590da092ddcf66a6df3f39f1a68f790a8b17
hanjungho <thegamerhan@gmail.com>
2025-02-01 12:58:52 +0900

    update

---
+++ kimyounghanstart/section06/Casting2.java
@@ -0,0 +1,13 @@
+package inflearn.kimyounghanstart.section06;
+
+public class Casting2 {
+
+    public static void main(String[] args) {
+        double doubleValue = 1.5;
+        int intValue = 0;
+
+//        intValue = doubleValue;
+        intValue = (int) doubleValue;
+        System.out.println("intValue = " + intValue);
+    }
+}

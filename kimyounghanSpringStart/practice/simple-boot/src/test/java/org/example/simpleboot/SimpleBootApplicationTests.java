7d315dbfdcce683a697c1264aec3986322da1387
hanjungho <thegamerhan@gmail.com>
2025-04-15 00:09:58 +0900

    250415

---
+++ kimyounghanSpringStart/practice/simple-boot/src/test/java/org/example/simpleboot/SimpleBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.simpleboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
